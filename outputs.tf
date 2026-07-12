50d29638aed4e161393f164f0d16e743e73ca03f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "dev_center_attached_networks" {
-  description = "All dev_center_attached_network resources"
-  value       = azurerm_dev_center_attached_network.dev_center_attached_networks
-}
 output "dev_center_attached_networks_dev_center_id" {
-  description = "List of dev_center_id values across all dev_center_attached_networks"
-  value       = [for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : v.dev_center_id]
+  description = "Map of dev_center_id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.dev_center_id }
 }
 output "dev_center_attached_networks_name" {
-  description = "List of name values across all dev_center_attached_networks"
-  value       = [for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : v.name]
+  description = "Map of name values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.name }
 }
 output "dev_center_attached_networks_network_connection_id" {
-  description = "List of network_connection_id values across all dev_center_attached_networks"
-  value       = [for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : v.network_connection_id]
+  description = "Map of network_connection_id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.network_connection_id }
 }
 
