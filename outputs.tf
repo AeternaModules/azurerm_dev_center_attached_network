2049fda7d54bc37bba34cb6e76b7c7ea81eac588
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "dev_center_attached_networks_id" {
+  description = "Map of id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.id }
+}
 output "dev_center_attached_networks_dev_center_id" {
   description = "Map of dev_center_id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
   value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.dev_center_id }
