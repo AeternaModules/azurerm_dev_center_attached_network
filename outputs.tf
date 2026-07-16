7c4991a72601c5659df73044d8724cab85f693ab
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "dev_center_attached_networks_id" {
   description = "Map of id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
-  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.id }
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "dev_center_attached_networks_dev_center_id" {
   description = "Map of dev_center_id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
-  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.dev_center_id }
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.dev_center_id if v.dev_center_id != null && length(v.dev_center_id) > 0 }
 }
 output "dev_center_attached_networks_name" {
   description = "Map of name values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
-  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.name }
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "dev_center_attached_networks_network_connection_id" {
   description = "Map of network_connection_id values across all dev_center_attached_networks, keyed the same as var.dev_center_attached_networks"
-  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.network_connection_id }
+  value       = { for k, v in azurerm_dev_center_attached_network.dev_center_attached_networks : k => v.network_connection_id if v.network_connection_id != null && length(v.network_connection_id) > 0 }
 }
 
